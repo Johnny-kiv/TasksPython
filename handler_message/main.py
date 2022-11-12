560766d49391194c32d0fc909e7def766596f21c
Johnny-kiv <johnny.kiv@yandex.ru>
2022-11-12 13:01:58 +0300

    fcg

--- handler_message/main.py
+++ handler_message/main.py
@@ -1,9 +1,12 @@
+import datetime
 while True:
     t = input().lower()
     if t=="привет":
         print("Здравствуй, хозяин!")
     elif t=="как дела?":
         print("Все чики-пуки, хозяин!")
+    elif t=="сколько времени?":
+        print(datetime.datetime.now().time())
     elif t=="0":
         print("Досвидос амигос")
         break
