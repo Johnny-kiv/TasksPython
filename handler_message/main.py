ce46a81547be1b36d992ceb58571eff9a7ebc168
Johnny-kiv <johnny.kiv@yandex.ru>
2022-11-12 13:10:01 +0300

    fcg

--- handler_message/main.py
+++ handler_message/main.py
@@ -1,4 +1,6 @@
 import datetime
+import random
+pogoda = ["Облачность","Солнечно","Дождь","Гроза"]
 while True:
     t = input().lower()
     if t=="привет":
@@ -7,6 +9,8 @@ while True:
         print("Все чики-пуки, хозяин!")
     elif t=="сколько времени?":
         print(datetime.datetime.now().time())
+    elif t=="какая погода?":
+        print("Температура:"+str(random.randint(-10,10))+"C,",pogoda[random.randint(0,3)],".")
     elif t=="0":
         print("Досвидос амигос")
         break
