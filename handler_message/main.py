637afe0e4c18e0db3606d0cdda86765f69b2da33
Johnny-kiv <johnny.kiv@yandex.ru>
2022-11-12 12:38:54 +0300

    fcg

--- handler_message/main.py
+++ handler_message/main.py
@@ -1,5 +1,10 @@
-t = input().lower()
-if t == "привет":
-    print("Здравствуй, хозяин!")
-else:
-    print("Error :(")
+while True:
+    t = input().lower()
+    if t=="привет":
+        print("Здравствуй, хозяин!")
+    elif t=="как дела?":
+        print("Все чики-пуки, хозяин!")
+    elif t=="0":
+        print("Досвидос амигос")
+    else:
+        print("Error :(")
