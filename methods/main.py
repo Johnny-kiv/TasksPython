7d9e52fa1543400c585668504bfa15735ac7d654
Johnny-kiv <johnny.kiv@yandex.ru>
2022-11-19 13:00:23 +0300

    $

--- methods/main.py
+++ methods/main.py
@@ -3,9 +3,9 @@ def CL(i):
     res = i.split(" ")
     b = res[0] > res[1]
     if b:
-        r = len(res[0])
+        r = res[0]
     else:
-        r = len(res[1])
+        r = res[1]
     return r
 i = input("Введите словечко: ")
 print(CL(i))
