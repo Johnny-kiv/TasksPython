ffeb82b5f0b32f61cd0cc8b9dff68108d44083f3
Johnny-kiv <johnny.kiv@yandex.ru>
2022-12-10 13:01:09 +0300

    ver2

--- handler_string/main.py
+++ handler_string/main.py
@@ -1,4 +1,4 @@
-l=0
+"""l=0
 p = "мама мыла раму"
 masi = []
 for i in range(len(p)):
@@ -8,5 +8,16 @@ for i in range(len(p)):
         masi.append(i)
     if p[i]=="а" or p[i]=="м":
         l+=1
-print(int(l/2))
-print(masi)
+b=True
+for i in masi:
+    if b==True:
+        print(p[i]+p[i+1],p[i])
+        b=False
+    else:
+        b=True"""
+l=0
+p = "мама мыла раму"
+masi = []
+for i in range(len(p)):
+    if p[i]=="а":
+        print(p[i-1]+p[i]+p[i+1])
