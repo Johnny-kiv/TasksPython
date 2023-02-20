0cd95e931331ae92250455b5c9b0386a9debe1ef
Johnny-kiv <johnny.kiv@yandex.ru>
2023-02-20 10:40:52 +0300

    1

---
+++ minmax1/main.py
@@ -0,0 +1,10 @@
+# Это задача на сортировку
+# Будем сортировать ВЫБОРОМ
+# This is a sorting task
+# I will sort by SELECT
+inp = open("input.txt")
+im = inp.readline().split(",")
+om = []
+index = 0
+for i in im:
+    minx = min(mas)
