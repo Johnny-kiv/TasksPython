01b5eee6f1a40a880160bf8eebbfa33b642cc508
Johnny-kiv <johnny.kiv@yandex.ru>
2023-02-21 21:57:48 +0300

    1

--- parser_bot/main.py
+++ parser_bot/main.py
@@ -5,7 +5,7 @@ import subprocess
 from bs4 import BeautifulSoup
 import requests
 ip = input()
-subprocess.run("pip install snscrape",shell=True)
+
 subprocess.run(f"snscrape --max-result 10 --jsonl telegram-channel {ip} > txt.txt",shell=True)
 inp = open("txt.txt")
 inp = inp.readlines()
@@ -16,5 +16,4 @@ for i in inp:
     src = BeautifulSoup(requests.get(url).text)
     print("################")
     statey = src.find(attrs={"data-post" : f"{ip}/{datapost}"})
-    for j in str(statey).split("<br>"):
-        print(j)
+    print(statey.text)
