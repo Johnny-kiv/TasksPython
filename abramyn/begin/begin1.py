40de2a81cd8b551007649d1c8881b99043fb4b16
Иван <johnny.kiv@yandex.ru>
2024-02-03 12:39:31 +0300

    begin2 - begin3

---
+++ abramyn/begin/begin1.py
@@ -0,0 +1,3 @@
+a = int(input("Enter the side of the square: "))
+p = a*4
+print("The perimeter of the square is",p)
---
+++ abramyn/begin/begin2.py
@@ -0,0 +1,3 @@
+a = int(input("Enter the side of square: "))
+s = a*a
+print("The area of the square is",s)
