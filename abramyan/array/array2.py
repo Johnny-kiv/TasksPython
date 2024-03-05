8b6039b79cb1c7410cbdea2983e560ae1996c15f
Иван <johnny.kiv@yandex.ru>
2024-03-05 13:05:35 +0300

    array2 is finised

--- abramyan/array/array2.py
+++ abramyan/array/array2.py
@@ -1,7 +1,7 @@
 n = int(input("Enter the number n: "))
 arr = []
-for i in range(1,n):
-    if i%2==1:
-        arr.append(i)
+for i in range(1,n+1):
+    num = 2**(i)
+    arr.append(num)
 for i in arr:
     print(i,end=" ")
--- abramyan/array/array5.py
+++ abramyan/array/array5.py
@@ -4,4 +4,4 @@ for i in range(2,n):
     f = array[i-2]+array[i-1]
     array.append(f)
 for i in array:
-    print(i,end=", ")
+    print(i,end=" ")
