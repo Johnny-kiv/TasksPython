06c983a9e831a25dfc7c4cd7f330ef715ffed2ff
Иван <johnny.kiv@yandex.ru>
2024-03-05 11:46:44 +0300

    array17

--- abramyan/array/array17.py
+++ abramyan/array/array17.py
@@ -1,11 +1,11 @@
+"""Дан массив A размера N. Вывести его элементы в следующем порядке:
+A1, A2, AN , AN−1, A3, A4, AN−2, AN−3, . . . .
+"""
 mas = []
-while True:
+n = int(input("Введите число N: "))
+for i in range(n):
     inp = input("Введите что-нибудь: ")
-    if inp == "break":
-        break
     mas.append(inp)
 l = len(mas)-1
 for i in range(0,l,2):
-    print(mas[i])
-    if not l-i<0:
-        print(mas[l-(i)])
+    print(mas[i],mas[i+1],mas[-i-1],end=" ")
