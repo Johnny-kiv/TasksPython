54d8b83463b92e6c20d7c4b19675860bfd413cc0
Иван <johnny.kiv@yandex.ru>
2024-04-05 21:46:04 +0300

    boolean20-21 are ready

---
+++ abramyan/boolean/boolean20.py
@@ -0,0 +1,2 @@
+A = input("Enter the number: ")
+print(A[0]!=A[1] and A[1]!=A[2] and A[0]!=A[2])
---
+++ abramyan/boolean/boolean21.py
@@ -0,0 +1,2 @@
+A = input("Enter the number: ")
+print(int(A[0])-int(A[1])==int(A[1])-int(A[2]) and int(A[0])-int(A[1])<0)
