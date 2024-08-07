435c62d0d884a2f08bc88b5712d22ff56054acae
Johnny-kiv <johnny.kiv@yandex.ru>
2024-08-07 22:33:29 +0300

    2-st task in progress

--- technocubF/1.py
+++ technocubF/1.py
@@ -1,16 +1,17 @@
-import time
-
 def printm(mas,res):
     for i in mas:
         res+=str(i)+" "
     return res
+
 if __name__=="__main__":
+
     a = int(input())
     b = int(input())
     c = int(input())
-    start_time = time.time()
+
     mas1 = [a,b,c]
     mas2 = []
+
     for i in mas1:
         f = i
         for j in mas1:
@@ -19,10 +20,9 @@ if __name__=="__main__":
         mas2.append(f)
         mas1.remove(f)
         print(len(mas1))
+
     mas2.append(mas1[0])
     mas1.pop(0)
-
-            
                 
     print(mas2)
     res = ""
@@ -40,5 +40,5 @@ if __name__=="__main__":
     else:
         print("No") 
             
-print("Process finished --- %s seconds ---" % (time.time() - start_time))
+
         
---
+++ technocubF/2.py
@@ -0,0 +1,8 @@
+import time
+if __name__ == "__main__":
+    mas = ["song.mp3", "problem.cfg","problem","picture.png","problem.cfg","sol123.c","data.dat","problem","sol13.c","sol123.c","song.mp3"]
+    ret = []
+    #n = int(input())
+    start = time.time()
+    for i
+    print(time.time()-start)
