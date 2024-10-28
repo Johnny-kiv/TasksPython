7d067d4432d174bd14aede27353cf8500b62e9f3
Johnny-kiv <johnny.kiv@yandex.ru>
2024-10-28 18:30:28 +0300

    123

--- test_shag_v_budushee/1.py
+++ test_shag_v_budushee/1.py
@@ -1,4 +1,4 @@
-mas = input().split(" ")
+"""mas = input().split(" ")
 a,c,m,x,n = int(mas[0]),int(mas[1]),int(mas[2]),int(mas[3]),int(mas[4])
 s = 0
 for i in range(n):
@@ -6,3 +6,20 @@ for i in range(n):
     s+=x
 s/=n
 print('%.4f'%s)
+"""
+n = input().split(" ")
+x = int(n[0])
+y = int(n[1])
+a = int(n[2])
+b = int(n[3])
+t = 0
+l = 0
+while x>y:
+    if l == 0:
+        y+=a
+        l=1
+    else:
+        x+=b
+        l=0
+    t+=1
+print(t)
--- test_shag_v_budushee/2.py
+++ test_shag_v_budushee/2.py
@@ -1,4 +1,4 @@
-e = "01234567"
+"""e = "01234567"
 inp = input()
 res = 'YES'
 for i in inp:
@@ -8,4 +8,33 @@ for i in inp:
 if res=="YES":
     if int(inp)%7!=0:
         res='NO'
+print(res)
+"""
+i1 = input().split(" ")
+i2 = input().split(" ")
+r1 = range(int(i2[0]),int(i2[1]))
+a = i1[0]
+b = i2[1]
+s1 = int(a[0])+int(a[1])+int(a[2])
+s2 = int(b[0])+int(b[1])+int(b[2])
+mas1 = []
+mas2 = []
+for i in r1:
+    n = str(i)
+    n = n[1:]
+    if n[0]=="0":
+        n = n[1:]
+    n = int(n)    
+    if n==s1 and int(a)<i and i<int(b):
+        mas1.append(i)
+        mas2.append("L")
+    elif n==s2 and int(a)<i and i<int(b):
+        mas1.append(i)
+        mas2.append("R")
+if mas2!=[]:
+    res1 = min(mas1)
+    res = mas2[mas1.index(res1)]+str(res1)
+    
+else:
+    res = 0
 print(res)
